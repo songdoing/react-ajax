2ac1569297d4f234f4b0a0065b58b3475035f530
wonjinsong <msmk22239@gmail.com>
2020-08-27 00:45:18 -0400

    Updates

---
+++ precache-manifest.b223949c956c3934e1df7349b464cac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cb8a6fd07baf0f337cd51b09b8f59e1",
+    "url": "/react-ajax/index.html"
+  },
+  {
+    "revision": "a8f126478946617a0e6e",
+    "url": "/react-ajax/static/css/main.92080801.chunk.css"
+  },
+  {
+    "revision": "b6d108de80d5af7dae34",
+    "url": "/react-ajax/static/js/2.8bb3a141.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ajax/static/js/2.8bb3a141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f126478946617a0e6e",
+    "url": "/react-ajax/static/js/main.41de3325.chunk.js"
+  },
+  {
+    "revision": "3b910da71623005c1088",
+    "url": "/react-ajax/static/js/runtime-main.9df8fd57.js"
+  }
+]);
